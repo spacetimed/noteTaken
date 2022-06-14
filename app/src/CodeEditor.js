c47050a2056c6446e5ab7878059d6e0b26c17325
FFFFFF-base16 <s127813127@gmail.com>
2022-06-13 18:31:56 -0700

    added line number

--- app/src/CodeEditor.js
+++ app/src/CodeEditor.js
@@ -1,5 +1,51 @@
 import { useState, useEffect } from 'react';
 
+function parse(markdownText) {
+    /*  Sample input:
+    -------------------------
+        this is just a test
+        i dont know
+        # one two three
+        ## four five
+        # s
+        **cool**
+        # **cool** right
+    -------------------------
+    */
+    let lines = markdownText.split('\n');
+    let returnStr = [];
+    lines.forEach( (line, index) => {
+        let tokenClass = "default";
+        const words = line.split(" ");
+        const tokenLead = words[0];
+        const tokenClassMap = {
+            "#" : "h1",
+            "##" : "h2",
+            "###" : "h3",
+            "####" : "h4",
+            "#####" : "h5",
+            "######" : "h6",
+        }
+
+        if(tokenLead in tokenClassMap)
+            tokenClass = tokenClassMap[tokenLead];
+
+        words.forEach( word => {
+            // Count occurrences of **, if even occurence, replace every other with open and close strong
+        });
+
+        returnStr.push(
+            <div key={index} className="line">
+                <span key={index + "_lineNum"} className="lineNumber">{index+1}</span>
+                <span key={index} className={tokenClass}>
+                    {line}
+                </span>
+            </div>
+        )
+    });
+    return returnStr;
+}
+
 function CodeEditor() {
 
     const [codeInput, setCodeChange] = useState(""); // State: { 'codeInput' : ... }
@@ -8,16 +54,14 @@ function CodeEditor() {
         // do something
     }, [codeInput]);
 
-    console.log('codeInput->', codeInput);
-
     return (
         <div className="masked-codeContainer">
             <div className="full-codeContainer">
                 <pre className="codeView">
-                    {codeInput}
-                    <br></br>
+                    {parse(codeInput)}
+                    <br />
                 </pre>
-                <textarea className="hiddenInput" onChange={(e) => setCodeChange(e.target.value)}></textarea>
+                <textarea spellCheck="false" className="hiddenInput" onChange={(e) => setCodeChange(e.target.value)}></textarea>
             </div>
         </div>
     )
