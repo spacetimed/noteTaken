cb9a5f8b2a2da55fa96cab59b2b6f00c274c8cf3
FFFFFF-base16 <s127813127@gmail.com>
2022-06-14 23:30:13 -0700

    fixed textarea tabbing

--- app/src/CodeEditor.js
+++ app/src/CodeEditor.js
@@ -47,25 +47,38 @@ function parse(markdownText) {
     return returnStr;
 }
 
-function handleKey(event, setCodeChange, codeInput) {
+function handleKey(event, setCodeChange, codeInput, codeInputElement, setCaretPos) {
     const key = event.keyCode;
     if(key == 9) { // Tab key
         event.preventDefault();
-        setCodeChange(codeInput + "    ");
+        const begin = codeInputElement.current.selectionStart;
+        const newCode = codeInput.substring(0, begin) + '    ' + codeInput.substring(begin, codeInput.length);
+        setCodeChange(newCode);
+        setCaretPos(begin + 4);
+        // caretPos(0);
     }
 }
 
 function CodeEditor({setHtmlContent}) {
 
-    const textInput = useRef(null);
+    const codeInputElement = useRef(null);
     const [codeInput, setCodeChange] = useState(""); // State: { 'codeInput' : ... }
+    const [caretPos, setCaretPos] = useState(-1);
 
     useEffect(() => {
         markdownToHtml(codeInput)
         .then( (htmlContent) => {
             setHtmlContent(htmlContent);
         });
+        if(caretPos !== -1) {
+            codeInputElement.current.selectionEnd = caretPos;
+            codeInputElement.current.selectionEnd = caretPos;
+            setCaretPos(-1);
+        }
     }, [codeInput]);
+
+    // useEffect( () => {
+    // }, [caretPos]);
 
     return (
         <div className="masked-codeContainer">
@@ -74,10 +87,11 @@ function CodeEditor({setHtmlContent}) {
                     {parse(codeInput)}
                 </pre>
                 <textarea autoFocus aria-hidden="true" spellCheck="false" 
+                    ref={codeInputElement}
                     value={codeInput} 
                     className="hiddenInput" 
                     onChange={(e) => setCodeChange(e.target.value)} 
-                    onKeyDown={(e) => handleKey(e, setCodeChange, codeInput)} 
+                    onKeyDown={(e) => handleKey(e, setCodeChange, codeInput, codeInputElement, setCaretPos)} 
                 />
             </div>
         </div>
