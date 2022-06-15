276ccce8e6d906cd721eba2287f955e443fd8fbb
FFFFFF-base16 <s127813127@gmail.com>
2022-06-15 04:45:19 -0700

    began mui drawer

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # NoteTaken
 
-A markdown note taker with basic VIM keybinds.
+A lightweight and minimalist **markdown note taker** for web browsers.
 
-This is meant to provide "quick note taking" with a markdown preview, offering an easy "copy to clipboard" option.
+This project is meant to provide "quick note taking" with a markdown preview, offering an easy "copy to clipboard" option prior to departure.
 
 ---
 
 ## Features: 
 * Dark theme style 
-* React, hooks
-* Sass/Scss
+* React
+* Sass
 * Material UI
 * [?] Host on Vercel / Heroku
+
+## Known bugs:
+* Cursor location is incorrect if word wrap occurs in the code editor.
+* Cursor location is incorrect on mobile.
--- app/src/App.js
+++ app/src/App.js
@@ -2,16 +2,73 @@ import './App.scss';
 import { useState, useEffect } from 'react';
 import CodeEditor from './CodeEditor';
 
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 function App() {
 
     const [htmlContent, setHtmlContent] = useState(""); // State: { 'htmlContent' : ... }
-
-    useEffect(() => {
-        // console.log('htmlContent->', htmlContent);
-    }, [htmlContent]);
+    const [drawerShow, setDrawer] = useState(true);
 
     return (
         <div className="App">
+            
+            <ThemeProvider theme={darkTheme}>
+                <Drawer
+                    open={drawerShow}
+                >
+                    <Box sx={{
+                        width: 250,
+                    }}>
+                        <List>
+                            <ListItem disablePadding sx={{paddingTop: '1rem'}}>
+                                <ListItemButton key="source">
+                                    <ListItemIcon>
+                                        <GitHubIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Source" />
+                                </ListItemButton>
+                            </ListItem>
+                            <ListItem disablePadding sx={{paddingBottom: '1rem'}}>
+                                <ListItemButton key="about">
+                                    <ListItemIcon>
+                                        <LibraryBooksIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="About" />
+                                </ListItemButton>
+                            </ListItem>
+                            <Divider />
+                            <ListItem disablePadding sx={{paddingTop: '1rem'}}>
+                                <ListItemButton key="copy">
+                                    <ListItemIcon>
+                                        <ContentPasteIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Copy Markdown" />
+                                </ListItemButton>
+                            </ListItem>
+                        </List>
+                    </Box>
+                </Drawer>
+            </ThemeProvider>
+
             <div className="sidebar"></div>
             <div className="contentGrid">
                 <CodeEditor setHtmlContent={setHtmlContent} />
